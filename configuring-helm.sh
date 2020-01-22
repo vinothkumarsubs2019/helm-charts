15b13dc78ec958d97cf9f39eb7a1e1cf7bf6f497
root <root@ip-172-31-84-22.ec2.internal>
2020-01-22 18:32:48 +0000

    adding files

---
+++ configuring-helm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install -y wget
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.12.2-linux-amd64.tar.gz
+tar -zxvf helm-v2.12.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+kubectl -n kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+kubectl create namespace kubeapps
